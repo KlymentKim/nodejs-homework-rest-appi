4add871f1508cdd674df7d0bc973f1b09e525d31
Klyment <klim86kr@gmail.com>
2023-08-28 17:49:59 +0300

    add helpers

---
+++ helpers/sendEmail.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const { SENDGRID_API_KEY } = process.env;
+
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const sendEmail = async (data) => {
+  const mail = { ...data, from: "kim.kliment.kr@gmail.com" };
+  try {
+    sgMail.send(mail);
+    return true;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = sendEmail;
+
+
